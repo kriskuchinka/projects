daa7e9c7ce64d3e05070ea4027d403b2ec8534ee
Kris Kuchinka <kriskuchinka@gmail.com>
2016-08-22 16:53:18 -0700

    Changing all pixel amounts to ems to make responsive design easier later.

--- potluck_party/js/scripts_potluck.js
+++ potluck_party/js/scripts_potluck.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
 
-
+	 // hide and show information when icon is clicked on 
 	$(".fa-info").click(function() {
 		$(".explaination-window").toggle({"display":"block"});
 		$("#explain-site").css({"height":"100%" , "width":"100%" , "font-size":"26px"});
 	});
 	
 
-	/* begin sign up, bring up modal window */
+	 // begin sign up, bring up modal window 
 	$("#modal-start").click(function(){
 		$("#modal-start").hide();
 		$("#modal1").show();
-		/* ensure user can't see other modal windows, in case they aren't on the first modal */
+		 // ensure user can't see other modal windows, in case they aren't on the first modal 
 		$("#modal2").hide();
 		$("#modal3").hide();
 		$("#welcome-par").hide();		
@@ -21,7 +21,7 @@ $(document).ready(function() {
 		$(".progress").css({"background-color":"red" , "width":"33%" , "border-radius":"10px" , "height":"24px"});
 	});
 	
-	/* give user exit option, show original starting point, remove progress bar */
+	 // give user exit option, show original starting point, remove progress bar 
 	$(".exit").click(function() {
 		$(".modal-window").hide();
 		$(".progress-bar").hide();
@@ -30,32 +30,28 @@ $(document).ready(function() {
 		$("#modal-start").show();
 	});
 
-	// $("#modal-start").click(function() {
-	//	$(".progress").css({"background-color":"red" , "width":"33%" , "border-radius":"10px" , "height":"24px"});
-	// });
-	
-	/* user option to move forward to 2nd modal window */
+	 // user option to move forward to 2nd modal window 
 	$("#change-1").click(function() {
 		$("#modal1").hide();
 		$("#modal2").show();
 		$(".progress").css({"background-color":"red" , "width":"66%" , "border-radius":"10px" , "height":"24px"});
 	});
 	
-	/* user option to switch to previous modal screen, progress bar regresses */
+	 // user option to switch to previous modal screen, progress bar regresses 
 	$("#reverse-1").click(function() {
 		$("#modal2").hide();
 		$("#modal1").show();
 		$(".progress").css({"background-color":"red" , "width":"33%" , "border-radius":"10px" , "height":"24px"});
 	});
 	
-	/* user option to forward to 3rd modal window */
+	 // user option to forward to 3rd modal window 
 	$("#change-2").click(function() {
 		$("#modal2").hide();
 		$("#modal3").show();
 		$(".progress").css({"width":"100%" , "background-color":"red" , "border-radius":"10px" , "height":"24px"});
 	});
 	
-	/* user option to switch to previous modal screen, progress bar regresses */
+	 // user option to switch to previous modal screen, progress bar regresses 
 	$("#reverse-2").click(function(){
 		$("#modal3").hide()
 		$("#modal2").show();
@@ -63,7 +59,7 @@ $(document).ready(function() {
 		$(".progress").css({"width":"66%" , "border-radius":"10px" , "height":"24px"});
 	});
 	
-	/* Capture form data and display on submit*/
+	 // Capture form data and display on submit
 	$("#change-3").click(function() {
 		$(".progress-bar").hide();
 		$("#modal3").hide();
@@ -91,22 +87,22 @@ $(document).ready(function() {
 		var organic;
 		var comments = $("#comments-suggestions").val();
 		
-		/* get values from check box */
-		/* gluten check box */
+		 // get values from check box 
+		 // gluten check box 
 		if ($(".gluten-check").prop("checked")) {
 			gluten = "Yes";
 		} else {
 		    gluten = "No";
 		}
 		
-		/* vegan check box */
+		 // vegan check box 
 		if ($(".vegan-check").prop("checked")) {
 			vegan = "Yes";
 		} else {
 			vegan = "No";
 		}
 		
-		/* organic check box */
+		 // organic check box 
 		if ($(".organic-check").prop("checked")) {
 			organic = "Yes";
 		} else {
@@ -114,9 +110,9 @@ $(document).ready(function() {
 		}
 
 	
-	/* display form information on final page */	
+	 // display form information on final page 	
 	 $("#form-data").html("<p> <span class='field-data'>Name:</span> " + fname + " " +  lname + "</p> <p> <span class='field-data'>Email: </span>" + email + "</p> <p> <span class='field-data'> Phone Number: </span>" + phone + "</p ><p> <span class='field-data'>Potluck Dish: </span>" + dish + "</p> <p> <span class='field-data'> Servings: <span>" + servings + "</p> <p> <span class='field-data'> Gluten Free: </span>" + gluten + "</p> <p> <span class='field-data'>Vegan: </span>" + vegan + "</p> <p> <span class='field-data'> Organic: </span>" + organic + "</p> <p> <span class='field-data'> Additional Notes: </span>" + comments + "</p>");
 	
 	}); // end of change-3 click
 	
-}); /* end of ready */
+ }); // end of ready 
